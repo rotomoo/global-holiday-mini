d7936c4ffdf1e5bf38621a1ab10fa4cd9155f7e7
rotomoo <quddlf410@naver.com>
2025-06-30 15:44:13 +0900

    chore : 프로젝트 셋팅

---
+++ src/main/java/com/globalholidaymini/GlobalHolidayMiniApplication.java
@@ -0,0 +1,13 @@
+package com.globalholidaymini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalHolidayMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalHolidayMiniApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/globalholidaymini/GlobalHolidayMiniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globalholidaymini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalHolidayMiniApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
